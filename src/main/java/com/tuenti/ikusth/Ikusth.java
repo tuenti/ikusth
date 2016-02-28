de3755ec9921ce970fdd314329895c3c9c9d2c2c
ivmos <ivanmosp@gmail.com>
2016-02-28 03:52:53 +0100

    Extract methods

--- src/main/java/com/tuenti/ikusth/Ikusth.java
+++ src/main/java/com/tuenti/ikusth/Ikusth.java
@@ -28,9 +28,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Ikusth extends Plugin implements ExternalPlugin {
-    private ThreadsDependencyGraph threadsDependencyGraph;
+    private static final String OUTPUT_FILENAME = "ikusth";
+    private static final String OUTPUT_TYPE = "png";
+    private static final String OUTPUT_DPI = "100";
     private Graphviz graphviz;
-    private String graphOutputFile;
 
     public Ikusth() {
     }
@@ -53,9 +54,7 @@ public class Ikusth extends Plugin implements ExternalPlugin {
 
     @Override
     public void reset() {
-        threadsDependencyGraph = null;
         graphviz = null;
-        graphOutputFile = null;
     }
 
     @Override
@@ -65,11 +64,51 @@ public class Ikusth extends Plugin implements ExternalPlugin {
     @Override
     public void generate(Module module) {
         BugReportModule bugReportModule = (BugReportModule)module;
-        this.threadsDependencyGraph = bugReportModule.getThreadsDependencyGraph();
 
+        Graph graph = getGraphvizGraph(bugReportModule.getThreadsDependencyGraph());
+
+        String graphOutputPathName = getGraphOutputPathName();
+        writeGraphToFile(getGraphviz().getGraphByteArray(graph, OUTPUT_TYPE, OUTPUT_DPI), new File(graphOutputPathName));
+        openOutputImageInBrowser(graphOutputPathName);
+    }
+
+    private String getGraphOutputPathName() {
+        return System.getProperty("java.io.tmpdir") + OUTPUT_FILENAME + "_" + System.currentTimeMillis() + "." + OUTPUT_TYPE;
+    }
+
+    private Graph getGraphvizGraph(ThreadsDependencyGraph threadsDependencyGraph) {
         Graph graph = new Graph("g1", GraphType.DIGRAPH);
         graph.addAttribute(new Attribute("rankdir", "LR"));
 
+        Map<String, Node> nodes = addNodesToGraph(threadsDependencyGraph, graph);
+        Map<String, Iterable<LabeledEdge>> threadDependencyMap = threadsDependencyGraph.getThreadDependencyMap();
+
+        List<String> deadlock = threadsDependencyGraph.getDeadLock();
+        for (Map.Entry<String, Iterable<LabeledEdge>> entry : threadDependencyMap.entrySet()) {
+            Node node = nodes.get(sanitizeForDot(entry.getKey()));
+            for (LabeledEdge labelEdge : entry.getValue()) {
+                Node toNode = nodes.get(sanitizeForDot(labelEdge.toName()));
+                Edge edge = new Edge(node, toNode);
+                addLabelToEdge(labelEdge, edge);
+                if (deadlock.contains(entry.getKey()) && deadlock.contains(labelEdge.toName())) {
+                    markEdgeAsRed(edge);
+                }
+
+                graph.addEdge(edge);
+            }
+        }
+        return graph;
+    }
+
+    private void addLabelToEdge(LabeledEdge labelEdge, Edge edge) {
+        edge.addAttribute(new Attribute("label", "\"" + getLabelLastPart(labelEdge) + "\""));
+    }
+
+    private void markEdgeAsRed(Edge edge) {
+        edge.addAttribute(new Attribute("color", "red"));
+    }
+
+    private Map<String, Node> addNodesToGraph(ThreadsDependencyGraph threadsDependencyGraph, Graph graph) {
         Iterable<String> threadNames = threadsDependencyGraph.getThreadNames();
         Map<String, Node> nodes = new HashMap<String, Node>();
         for (String threadName : threadNames) {
@@ -77,29 +116,7 @@ public class Ikusth extends Plugin implements ExternalPlugin {
             nodes.put(this.sanitizeForDot(threadName), node);
             graph.addNode(node);
         }
-
-        Map<String, Iterable<LabeledEdge>> threadDependencyMap = threadsDependencyGraph.getThreadDependencyMap();
-        List<String> deadlock = threadsDependencyGraph.getDeadLock();
-        for (Map.Entry<String, Iterable<LabeledEdge>> entry : threadDependencyMap.entrySet()) {
-            Node node = nodes.get(sanitizeForDot(entry.getKey()));
-            for (LabeledEdge labelEdge : entry.getValue()) {
-                Node toNode = nodes.get(sanitizeForDot(labelEdge.toName()));
-                Edge edge = new Edge(node, toNode);
-                edge.addAttribute(new Attribute("label", "\"" + getLabelLastPart(labelEdge) + "\""));
-                if (deadlock.contains(entry.getKey()) && deadlock.contains(labelEdge.toName())) {
-                    edge.addAttribute(new Attribute("color", "red"));
-                }
-
-                graph.addEdge(edge);
-            }
-        }
-        String type = "png";
-        graphOutputFile = "/tmp/outEX1."+ type;
-        // TODO add random to file name
-        File out = new File(graphOutputFile);
-        this.writeGraphToFile(getGraphviz().getGraphByteArray(graph, type, "100"), out );
-
-        openBrowserIfNeeded();
+        return nodes;
     }
 
     private Graphviz getGraphviz() {
@@ -114,31 +131,32 @@ public class Ikusth extends Plugin implements ExternalPlugin {
         return fullLabel[fullLabel.length - 1];
     }
 
+    /**
+     * Adapts input String to something suitable for GraphViz node name.
+     * @param input
+     * @return String
+     */
     private String sanitizeForDot(String input) {
         return input.replace("-","_").replace(" ", "").replace("(","").replace(")","");
     }
 
-    private void openBrowserIfNeeded() {
-        if (graphOutputFile != null) {
-            try {
-                File file = new File(graphOutputFile);
-                System.out.println("Launching browser with URI: " + file.toURI());
-                java.awt.Desktop.getDesktop().browse(file.toURI());
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
+    private void openOutputImageInBrowser(String graphOutputPathName) {
+        try {
+            File file = new File(graphOutputPathName);
+            System.out.println("Launching browser with URI: " + file.toURI());
+            java.awt.Desktop.getDesktop().browse(file.toURI());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public int writeGraphToFile(byte[] img, File to)
-    {
+    public int writeGraphToFile(byte[] img, File to) {
         try {
             FileOutputStream fos = new FileOutputStream(to);
             fos.write(img);
             fos.close();
         } catch (java.io.IOException ioe) {
-            // Do nothing
+            ioe.printStackTrace();
         }
         return 1;
     }
